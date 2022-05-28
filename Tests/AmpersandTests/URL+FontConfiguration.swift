cf6c25a8418620fe66dc2ca9ebd4575f80307bfb
Stephen Anthony <steve@darjeelingapps.com>
2022-05-28 15:26:50 +0100

    Add xcodeproj

--- Tests/AmpersandTests/URL+FontConfiguration.swift
+++ Tests/AmpersandTests/URL+FontConfiguration.swift
@@ -9,8 +9,12 @@ import Foundation
 
 extension URL {
     static func urlForConfigurationFile(withName name: String) -> URL {
+#if SWIFT_PACKAGE
         let sourceFileURL = URL(fileURLWithPath: #file)
         let sourceFileDirectory = sourceFileURL.deletingLastPathComponent()
         return sourceFileDirectory.appendingPathComponent("Font Configuration Files").appendingPathComponent(name).appendingPathExtension("json")
+#else
+        return Bundle(for: FontProviderTests.self).url(forResource: name, withExtension: "json")
+#endif
     }
 }
